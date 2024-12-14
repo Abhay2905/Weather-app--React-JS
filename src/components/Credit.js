33dba2d08969c086f1bba9af0dab3a59b6c0d26b
Abhay <74758896+Abhay2905@users.noreply.github.com>
2024-12-14 14:15:32 +0530

    Add files via upload

---
+++ src/components/Credit.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href=""> Abhay Valand</a>, <a href="h">Open sourced on Github.</a> 
+    </p>
+  </div>
+  );
+}
